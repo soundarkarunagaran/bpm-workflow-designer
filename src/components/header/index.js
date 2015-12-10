8af97e85228a5b2ca593e30c010326ebf2c440f0
chenxuejia <chenxuejia@idcos.com>
2015-12-10 11:32:46 +0800

    添加编辑导航 UI

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,9 +10,65 @@ var Header = React.createClass({
 					workflow
 				</div>
 				<div className="block menu-container content">
-					menus
+					<Menu/>
 				</div>
 			</header>
+		)
+	}
+})
+
+var Menu = React.createClass({
+	render() {
+		return (
+			<div className="menu-list">
+				<div className="menu-item">
+					<a className="mbtn mbtn-circle mbtn-icon-only"> <i className="fa fa-arrow-left"></i></a>
+				</div>
+				<div className="menu-item">
+					<a className="mbtn mbtn-circle mbtn-icon-only"><i className="fa fa-arrow-right"></i></a>
+				</div>
+				<div className="menu-item">
+					<a className="mbtn mbtn-circle mbtn-icon-only"><i className="fa fa-trash"></i></a>
+				</div>
+				<div className="menu-gap"></div>
+				<div className="menu-item icon-up">
+					<i className="fa fa-object-group"></i> <br/>
+					<label>分组</label>
+				</div>
+				<div className="menu-item icon-up">
+					<i className="fa fa-object-ungroup"></i> <br/>
+					<label>取消分组</label>
+				</div>
+				<div className="menu-gap"></div>
+				<div className="menu-item icon-up">
+					<i className="fa fa-cut"></i> <br/>
+					<label>剪切</label>
+				</div>
+				<div className="menu-item icon-up">
+					<i className="fa fa-copy"></i> <br/>
+					<label>复制</label>
+				</div>
+				<div className="menu-item icon-up">
+					<i className="fa fa-paste"></i> <br/>
+					<label>粘贴</label>
+				</div>
+				<div className="menu-gap"></div>
+				<div className="menu-item">
+					<a className="mbtn mbtn-circle mbtn-icon-only"><i className="fa fa-eye"></i></a>
+				</div>
+				<div className="menu-gap"></div>
+				<div className="menu-group pull-right">
+					<div className="menu-item">
+						<a className="mbtn mbtn-circle"> <i className="fa fa-send"/> 导出</a>
+					</div>
+					<div className="menu-item">
+						<a className="mbtn mbtn-circle"> <i className="fa fa-rocket"/> 部署</a>
+					</div>
+					<div className="menu-item">
+						<a className="mbtn mbtn-circle mbtn-primary "> <i className="fa fa-save"/> 保存</a>
+					</div>
+				</div>
+			</div>
 		)
 	}
 })
